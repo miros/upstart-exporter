85c9788d77f583866b31105669937684527ca0bc
m.zavyrylin <m.zavyrylin@fun-box.ru>
2014-02-11 13:45:17 +0400

    CNAV-1559 Renamed a private method

--- lib/upstart-exporter/exporter_helpers.rb
+++ lib/upstart-exporter/exporter_helpers.rb
@@ -1,7 +1,7 @@
 class Upstart::Exporter
   module ExporterHelpers
     def export_cmd_helper(cmd_name, cmd, binds={})
-      helper_script_cont = Templates.helper helper_binds(cmd, binds)
+      helper_script_cont = Templates.helper append_cmd(cmd, binds)
       File.open(helper_cmd_conf(cmd_name), 'w') do |f|
         f.write(helper_script_cont)
       end
@@ -25,7 +25,7 @@ class Upstart::Exporter
 
     private
 
-    def helper_binds(cmd, binds)
+    def append_cmd(cmd, binds)
       return binds unless cmd
 
       *start, tail = cmd.split /\s*(&&|\|\|)\s*/
